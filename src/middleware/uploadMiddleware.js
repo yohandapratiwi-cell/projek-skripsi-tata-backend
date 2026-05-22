b8614427168a58271101e6eab79c8e32650dd7cb
yohandagita25 <yohandapratiwi8@gmail.com>
2026-05-23 00:51:34 +0700

    fix upload contol

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -1,9 +1,10 @@
 const multer = require("multer");
 const path = require("path");
+const os = require("os"); // ✅ 1. Import modul bawaan Node.js untuk mendeteksi folder Temp
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // 🔥 ini folder tujuan
+    cb(null, os.tmpdir()); // 🔥 2. Ganti "uploads/" menjadi os.tmpdir()
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
